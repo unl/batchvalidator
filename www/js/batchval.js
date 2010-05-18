b09467e708a1c6ec5063b0e38473eae10c0e7629
Seth Meranda <smeranda2@unl.edu>
2010-05-18 19:50:23 +0000

    JS file on its own.

---
+++ www/js/batchval.js
@@ -0,0 +1,56 @@
+function validateAll()
+{
+	WDN.jQuery('.uri').each(function(){
+		// Grab the URI
+		var uri = WDN.jQuery(this).html();
+		
+		var uriDiv = WDN.jQuery(this).parent();
+		
+		WDN.jQuery('body').queue('validation', function() {
+			checkValidity(uri, uriDiv)
+		});
+	});
+	WDN.jQuery('body').dequeue('validation');
+}
+
+function validateInvalid()
+{
+	WDN.jQuery('.false .uri').each(function(){
+		// Grab the URI
+		var uri = WDN.jQuery(this).html();
+		var uriDiv = WDN.jQuery(this).parent();
+		WDN.jQuery('body').queue('validation', function() {
+			checkValidity(uri, uriDiv)
+		});
+	});
+	WDN.jQuery('body').dequeue('validation');
+}
+
+function checkValidity(uri, uriDiv)
+{
+	uriDiv.removeClass('true false')
+	// Tell the user we're loading the result
+	uriDiv.append('<img class="loading" src="/wdn/templates_3.0/css/header/images/colorbox/loading.gif" />');
+	// Fetch the validator results in JSON format.
+	WDN.get('validator.php?base='+baseURI+'&u='+escape(uri), null, function(result) {
+		handleJSONResult(result, uriDiv);
+	}, 'json');
+}
+
+function handleJSONResult(result, uriDiv)
+{
+	WDN.log(result);
+	uriDiv.children('.loading').remove();
+	
+	// Advance the queue
+	WDN.jQuery('body').dequeue('validation');
+	
+	if (result.validity) {
+		// It is valid, say no more!
+		uriDiv.addClass('true');
+		return;
+	}
+
+	uriDiv.addClass('false');
+
+}
