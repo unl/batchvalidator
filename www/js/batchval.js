547a1e30390116dfb62da7ae2d752bec88d9754f
Seth Meranda <smeranda2@unl.edu>
2010-05-21 17:47:59 +0000

    UI updates, show error and warning totals, allow user to click these and showcase the errors/warnings in a colorbox.

--- www/js/batchval.js
+++ www/js/batchval.js
@@ -61,6 +61,17 @@ function handleJSONResult(result, uriDiv)
 	
 	uriDiv.removeClass('unknown true');
 	uriDiv.addClass('false');
-	uriDiv.children('span').append("<a href='#' class='errors'>"+result.errors.length+" Error(s)</a> <a href='#' class='warnings'>"+result.warnings.length+" Warning(s)</a>");
-
+	uriDiv.children('span').append('<a href="#" class="errors" onclick="showResults(\''+uriDiv.attr("id")+'\'); return false;">'+result.errors.length+' Error(s)</a> <a href="#" class="errors" onclick="showResults(\''+uriDiv.attr("id")+'\'); return false;">'+result.warnings.length+' Warning(s)</a>');
+	uriDiv.append("<div class='details'></div>");
+	uriDiv.children('.details').append("<div class='errorDetails'><h3 class='sec_main'>Errors</h3></div>");
+	for (var j=0; j<result.errors.length; j++) {
+		uriDiv.children('.details').children('.errorDetails').append("<div><h4><em>Line "+result.errors[j].line +", Column "+ result.errors[j].col + ":</em> "+ result.errors[j].message +"</h4><pre>"+ result.errors[j].source + "</pre>" + result.errors[j].explanation + "</div>");
+	}
+	uriDiv.children('.details').append("<div class='warningDetails'><h3 class='sec_main'>Warnings</h3></div>");
+	for (var i=0; i<result.warnings.length; i++) {
+		uriDiv.children('.details').children('.warningDetails').append("<div><h4><em>Line "+result.warnings[i].line +", Column "+ result.warnings[i].col + ":</em> "+ result.warnings[i].message +"</h4><pre>"+ result.warnings[i].source + "</pre>" + result.warnings[i].explanation + "</div>");
+	}
+}
+function showResults(errorDiv) {
+	WDN.jQuery('#'+errorDiv).colorbox({inline:true, maxWidth:'940px', height:'60%', href:'#'+errorDiv+" .details"});
 }
