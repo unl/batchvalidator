c5e60c1c765860cea64c142363fb29f568c62361
Brett Bieber <brett.bieber@gmail.com>
2012-03-27 13:56:08 +0000

    Upgrade to UNL 3.1 responsive template.

--- www/index.php
+++ www/index.php
@@ -21,69 +21,86 @@ if (isset($_GET['uri'])
     }
 }
 ?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en"><!-- InstanceBegin template="/Templates/php.fixed.dwt.php" codeOutsideHTMLIsLocked="false" -->
+<!DOCTYPE html>
+<!--[if IEMobile 7 ]><html class="ie iem7"><![endif]-->
+<!--[if lt IE 7 ]><html class="ie ie6" lang="en"><![endif]-->
+<!--[if IE 7 ]><html class="ie ie7" lang="en"><![endif]-->
+<!--[if IE 8 ]><html class="ie ie8" lang="en"><![endif]-->
+<!--[if (gte IE 9)|(gt IEMobile 7) ]><html class="ie" lang="en"><![endif]-->
+<!--[if !(IEMobile) | !(IE)]><!--><html lang="en"><!-- InstanceBegin template="/Templates/php.fixed.dwt.php" codeOutsideHTMLIsLocked="false" --><!--<![endif]-->
 <head>
+<?php virtual("/wdn/templates_3.1/includes/metanfavico.html"); ?>
 <!--
     Membership and regular participation in the UNL Web Developer Network
-    is required to use the UNL templates. Visit the WDN site at 
+    is required to use the UNL templates. Visit the WDN site at
     http://wdn.unl.edu/. Click the WDN Registry link to log in and
     register your unl.edu site.
     All UNL template code is the property of the UNL Web Developer Network.
-    The code seen in a source code view is not, and may not be used as, a 
-    template. You may not use this code, a reverse-engineered version of 
+    The code seen in a source code view is not, and may not be used as, a
+    template. You may not use this code, a reverse-engineered version of
     this code, or its associated visual presentation in whole or in part to
     create a derivative work.
     This message may not be removed from any pages based on the UNL site template.
-    
-    $Id: php.fixed.dwt.php 536 2009-07-23 15:47:30Z bbieber2 $
+
+    $Id: php.fixed.dwt.php | ebd6eef8f48e609f4e2fe9c1d6432991649298e7 | Tue Mar 6 14:38:44 2012 -0600 | Brett Bieber  $
 -->
-<link rel="stylesheet" type="text/css" media="screen" href="/wdn/templates_3.0/css/all.css" />
-<link rel="stylesheet" type="text/css" media="print" href="/wdn/templates_3.0/css/print.css" />
-<script type="text/javascript" src="/wdn/templates_3.0/scripts/all.js"></script>
-<?php virtual('/wdn/templates_3.0/includes/browserspecifics.html'); ?>
-<?php virtual('/wdn/templates_3.0/includes/metanfavico.html'); ?>
+<?php virtual("/wdn/templates_3.1/includes/scriptsandstyles.html"); ?>
 <!-- InstanceBeginEditable name="doctitle" -->
-<title>UNL | WDN | Batch Validator</title>
-<!-- InstanceEndEditable --><!-- InstanceBeginEditable name="head" -->
-<link rel="stylesheet" type="text/css" href="/wdn/templates_3.0/css/content/zenform.css" />
+<title>Batch Validator | University of Nebraska&ndash;Lincoln</title>
+<!-- InstanceEndEditable -->
+<!-- InstanceBeginEditable name="head" -->
+<!-- Place optional header elements here -->
+<link rel="stylesheet" type="text/css" href="/wdn/templates_3.1/css/content/zenform.css" />
 <link rel="stylesheet" type="text/css" href="css/batchval.css" />
 <script type="text/javascript" src="js/batchval.js"></script>
 <script type="text/javascript">var baseURI = '<?php echo $uri; ?>';</script>
+
 <!-- InstanceEndEditable -->
+<!-- InstanceParam name="class" type="text" value="fixed" -->
 </head>
-<body class="fixed">
-<p class="skipnav"> <a class="skipnav" href="#maincontent">Skip Navigation</a> </p>
-<div id="wdn_wrapper">
-    <div id="header"> <a href="http://www.unl.edu/" title="UNL website"><img src="/wdn/templates_3.0/images/logo.png" alt="UNL graphic identifier" id="logo" /></a>
-        <h1>University of Nebraska&ndash;Lincoln</h1>
-        <?php virtual('/wdn/templates_3.0/includes/wdnTools.html'); ?>
-    </div>
-    <div id="wdn_navigation_bar">
-        <div id="breadcrumbs">
-            <!-- WDN: see glossary item 'breadcrumbs' -->
-            <!-- InstanceBeginEditable name="breadcrumbs" -->
-            <ul>
-                <li class="first"><a href="http://www.unl.edu/">UNL</a></li>
-                <li><a href="http://wdn.unl.edu/"><acronym title="Web Developer Network">WDN</acronym></a></li>
-                <li>Batch Validator</li>
-            </ul>
-            <!-- InstanceEndEditable --></div>
-        <div id="wdn_navigation_wrapper">
-            <div id="navigation"><!-- InstanceBeginEditable name="navlinks" -->
-                <?php echo file_get_contents('http://www1.unl.edu/wdn/templates_3.0/scripts/navigationSniffer.php?u=http://wdn.unl.edu/'); ?>
-                <!-- InstanceEndEditable --></div>
+<body class="fixed" data-version="3.1">
+    <nav class="skipnav">
+        <a class="skipnav" href="#maincontent">Skip Navigation</a>
+    </nav>
+    <div id="wdn_wrapper">
+        <header id="header" role="banner">
+            <a id="logo" href="http://www.unl.edu/" title="UNL website">UNL</a>
+            <span id="wdn_institution_title">University of Nebraska&ndash;Lincoln</span>
+            <span id="wdn_site_title"><!-- InstanceBeginEditable name="titlegraphic" -->The Title of My Site<!-- InstanceEndEditable --></span>
+            <?php virtual("/wdn/templates_3.1/includes/idm.html"); ?>
+            <?php virtual("/wdn/templates_3.1/includes/wdnTools.html"); ?>
+        </header>
+        <div id="wdn_navigation_bar">
+            <nav id="breadcrumbs">
+                <!-- WDN: see glossary item 'breadcrumbs' -->
+                <h3 class="wdn_list_descriptor hidden">Breadcrumbs</h3>
+                <!-- InstanceBeginEditable name="breadcrumbs" -->
+                <ul>
+                    <li class="first"><a href="http://www.unl.edu/">UNL</a></li>
+                    <li><a href="http://wdn.unl.edu/"><abbr title="Web Developer Network">WDN</abbr></a></li>
+                    <li>Batch Validator</li>
+                </ul>
+                <!-- InstanceEndEditable -->
+            </nav>
+            <div id="wdn_navigation_wrapper">
+                <nav id="navigation" role="navigation">
+                    <h3 class="wdn_list_descriptor hidden">Navigation</h3>
+                    <!-- InstanceBeginEditable name="navlinks" -->
+                    <?php echo file_get_contents('http://www1.unl.edu/wdn/templates_3.0/scripts/navigationSniffer.php?u=http://wdn.unl.edu/'); ?>
+                    <!-- InstanceEndEditable -->
+                </nav>
+            </div>
         </div>
-    </div>
-    <div id="wdn_content_wrapper">
-        <div id="titlegraphic"><!-- InstanceBeginEditable name="titlegraphic" -->
-            <h1>Batch Validator</h1>
-            <!-- InstanceEndEditable --></div>
-        <div id="pagetitle"><!-- InstanceBeginEditable name="pagetitle" --> <!-- InstanceEndEditable --></div>
-        <div id="maincontent">
-            <!--THIS IS THE MAIN CONTENT AREA; WDN: see glossary item 'main content area' -->
-            <!-- InstanceBeginEditable name="maincontentarea" -->
-            <form method="get" action="" class="zenform primary" style="width:930px;margin-top:10px;">
+        <div id="wdn_content_wrapper">
+            <div id="pagetitle">
+                <!-- InstanceBeginEditable name="pagetitle" -->
+                <h1>Batch Validator</h1>
+                <!-- InstanceEndEditable -->
+            </div>
+            <div id="maincontent" role="main">
+                <!--THIS IS THE MAIN CONTENT AREA; WDN: see glossary item 'main content area' -->
+                <!-- InstanceBeginEditable name="maincontentarea" -->
+                <form method="get" action="" class="zenform primary" style="width:930px;margin-top:10px;">
                     <fieldset>
                         <legend>Submit your site for validation</legend>
                         <ol>
@@ -177,20 +194,23 @@ if (isset($_GET['uri'])
                </ul>
             
             </div>
-            <!-- InstanceEndEditable -->
-            <div class="clear"></div>
-            <?php virtual('/wdn/templates_3.0/includes/noscript.html'); ?>
-            <!--THIS IS THE END OF THE MAIN CONTENT AREA.-->
-        </div>
-        <div id="footer">
-            <div id="footer_floater"></div>
-            <div class="footer_col">
-                <?php virtual('/wdn/templates_3.0/includes/feedback.html'); ?>
+                <!-- InstanceEndEditable -->
+                <div class="clear"></div>
+                <?php virtual("/wdn/templates_3.1/includes/noscript.html"); ?>
+                <!--THIS IS THE END OF THE MAIN CONTENT AREA.-->
             </div>
-            <div class="footer_col"><!-- InstanceBeginEditable name="leftcollinks" -->
+        </div>
+        <footer id="footer">
+            <div id="footer_floater"></div>
+            <div class="footer_col" id="wdn_footer_feedback">
+                <?php virtual("/wdn/templates_3.1/includes/feedback.html"); ?>
+            </div>
+            <div class="footer_col" id="wdn_footer_related">
+                <!-- InstanceBeginEditable name="leftcollinks" -->
                 <?php echo file_get_contents('http://wdn.unl.edu/sharedcode/relatedLinks.html'); ?>
                 <!-- InstanceEndEditable --></div>
-            <div class="footer_col"><!-- InstanceBeginEditable name="contactinfo" -->
+            <div class="footer_col" id="wdn_footer_contact">
+                <!-- InstanceBeginEditable name="contactinfo" -->
                 <h3>Contacting Us</h3>
                 <p>
                 The WDN is coordinated by:<br />
@@ -199,18 +219,21 @@ if (isset($_GET['uri'])
                 WICK 17<br />
                 Lincoln, NE 68583-0218</p>
                 <!-- InstanceEndEditable --></div>
-            <div class="footer_col">
-                <?php virtual('/wdn/templates_3.0/includes/socialmediashare.html'); ?>
+            <div class="footer_col" id="wdn_footer_share">
+                <?php virtual("/wdn/templates_3.1/includes/socialmediashare.html"); ?>
             </div>
-            <!-- InstanceBeginEditable name="optionalfooter" --> <!-- InstanceEndEditable -->
-            <div id="wdn_copyright"><!-- InstanceBeginEditable name="footercontent" -->
-                <?php file_get_contents('http://wdn.unl.edu/sharedcode/footer.html'); ?>
-                <!-- InstanceEndEditable -->
-                <?php virtual('/wdn/templates_3.0/includes/wdn.html'); ?>
-                | <a href="http://validator.unl.edu/check/referer">W3C</a> | <a href="http://jigsaw.w3.org/css-validator/check/referer?profile=css3">CSS</a> <a href="http://www.unl.edu/" title="UNL Home" id="wdn_unl_wordmark"><img src="/wdn/templates_3.0/css/footer/images/wordmark.png" alt="UNL's wordmark" /></a> </div>
-        </div>
+            <!-- InstanceBeginEditable name="optionalfooter" -->
+            <!-- InstanceEndEditable -->
+            <div id="wdn_copyright">
+                <div>
+                    <!-- InstanceBeginEditable name="footercontent" -->
+                    <?php file_get_contents('http://wdn.unl.edu/sharedcode/footer.html'); ?>
+                    <!-- InstanceEndEditable -->
+                    <?php virtual("/wdn/templates_3.1/includes/wdn.html"); ?>
+                </div>
+                <?php virtual("/wdn/templates_3.1/includes/logos.html"); ?>
+            </div>
+        </footer>
     </div>
-    <div id="wdn_wrapper_footer"> </div>
-</div>
 </body>
 <!-- InstanceEnd --></html>
