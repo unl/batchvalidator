e12c1887ee975468f2bd32fc2256f8897de03991
Michael Fairchild <mfairchild365@gmail.com>
2013-03-08 14:32:32 -0600

    Increase the time limit of the link checker
    Was throwing a fatal error on link farms.  Make the time limit configurable.

--- src/UNL/WDN/Assessment/LinkChecker.php
+++ src/UNL/WDN/Assessment/LinkChecker.php
@@ -2,6 +2,8 @@
 class UNL_WDN_Assessment_LinkChecker extends Spider_LoggerAbstract
 {
     protected static $checked = array();
+    
+    public static $time_limit = 60;
 
     /**
      *
@@ -24,6 +26,8 @@ class UNL_WDN_Assessment_LinkChecker extends Spider_LoggerAbstract
     
     function checkLinks($uri, $links, $depth)
     {
+        set_time_limit(self::$time_limit);
+        
         $mcurl = curl_multi_init();
         $curl = array();
         $activeRequests = 0;
