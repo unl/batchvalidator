7065b4318543c593fcdf81ae97173d6655f4f833
Seth Meranda <seth@meranda.org>
2013-03-08 21:47:38 -0600

    Add Handelbars helpers for formatting.

--- www/js/main.js
+++ www/js/main.js
@@ -28,4 +28,54 @@ WDN.loadJQuery(function() {
     }(WDN.jQuery));
 
     validator.initialize();
+// Handlebars helpers
+Handlebars.registerHelper('percentage', function (current, total) {
+    var percentage = Math.round(current/total * 100);
+    if (isNaN(percentage)) {
+        return '!';
+    }
+    return percentage + "%";
+});
+
+Handlebars.registerHelper('strip_site', function (page) {
+    var site = WDN.jQuery("#uri").val();
+    return page.replace(site, "/"); //Keep the slash?
+});
+
+Handlebars.registerHelper('format_boolean', function (marker) {
+    if (!marker) {
+        return '&#x2716;';
+    }
+    return "&#x2714;";
+});
+
+Handlebars.registerHelper('links', function (links) {
+    var total = 0;
+    if (links["301"]){
+        total = links["301"].length;
+    }
+    if (links["404"]) {
+        total = total + links["404"].length;
+    }
+    return total;
+});
+
+// Error Reporting
+Handlebars.registerHelper('error_percentage', function (current, total) {
+    var percentage = Math.round(current/total * 100);
+    if (isNaN(percentage) || percentage < 100) {
+        return 'error';
+    }
+});
+
+Handlebars.registerHelper('error_total', function (total) {
+    if (parseFloat(total) !== 0) {
+        return 'error';
+    }
+});
+
+Handlebars.registerHelper('error_boolean', function (marker) {
+    if (!marker) {
+        return 'error';
+    }
 });
