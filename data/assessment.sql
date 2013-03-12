f18ca4f9af5ab8bb2a15b92b07d5272fcf326b57
Michael Fairchild <mfairchild365@gmail.com>
2013-03-12 11:05:22 -0500

    delete children on deletion of parent

--- data/assessment.sql
+++ data/assessment.sql
@@ -7,7 +7,7 @@ CREATE TABLE IF NOT EXISTS `assessment` (
   `template_html` varchar(10) DEFAULT 'unknown' NOT NULL,
   `timestamp` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
   PRIMARY KEY (`baseurl`,`url`(500)),
-  CONSTRAINT FOREIGN KEY (`baseurl`) REFERENCES `assessment_runs`(`baseurl`),
+  CONSTRAINT FOREIGN KEY (`baseurl`) REFERENCES `assessment_runs`(`baseurl`) ON DELETE CASCADE,
   KEY `html_errors` (`html_errors`),
   KEY `accessibility_errors` (`accessibility_errors`),
   KEY `template_dep` (`template_dep`),
--- data/url_has_badlinks.sql
+++ data/url_has_badlinks.sql
@@ -5,7 +5,7 @@ CREATE TABLE IF NOT EXISTS `url_has_badlinks` (
   `code` int NOT NULL,
   `timestamp` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
   PRIMARY KEY (`baseurl`,`url`(500),`link_url`(500)),
-  CONSTRAINT FOREIGN KEY (`baseurl`) REFERENCES `assessment_runs`(`baseurl`),
+  CONSTRAINT FOREIGN KEY (`baseurl`) REFERENCES `assessment_runs`(`baseurl`) ON DELETE CASCADE,
   KEY `url` (`url`),
   KEY `code` (`code`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1; 
