c87b9094663e5c92fa5e4512abef096cb1dc547c
Michael Fairchild <mfairchild365@gmail.com>
2013-03-14 16:29:53 -0500

    add a script to test emails

---
+++ scripts/sendTestEmail.php
@@ -0,0 +1,12 @@
+<?php
+
+require dirname(dirname(__FILE__)) . '/www/config.inc.php';
+
+if (!isset($argv[1]) || !isset($argv[2])) {
+    echo "useage: php sendTestEmail.php http://reportsite.unl.edu/ youremail@unl.edu" . PHP_EOL;
+    exit();
+}
+
+$assessment = new UNL_WDN_Assessment($argv[1], $db);
+
+$assessment->emailStats($argv[2]);
