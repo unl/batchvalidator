98958ee58b548032e18096902fceb309f973f4f9
Michael Fairchild <mfairchild365@gmail.com>
2013-03-14 14:13:34 -0500

    add scripts for auto-queue

---
+++ scripts/auto_queue.php
@@ -0,0 +1,11 @@
+<?php
+require_once dirname(dirname(__FILE__)) . '/www/config.inc.php';
+
+//Only run a max of 5 checks at a time.
+$sth = $db->prepare("select site.baseurl, date_completed from site LEFT JOIN assessment_runs on assessment_runs.baseurl = site.baseurl ORDER BY date_completed ASC LIMIT 5");
+$sth->execute();
+
+while ($result = $sth->fetch()) {
+    $assessment = new UNL_WDN_Assessment($result['baseurl'], $db);
+    $assessment->addRun();
+}
