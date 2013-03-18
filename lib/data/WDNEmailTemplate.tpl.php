d3c68a52632d248cee7e03beb4bbfb932bb21724
Michael Fairchild <mfairchild365@gmail.com>
2013-03-18 10:54:57 -0500

    Update the WDN email Template

--- lib/data/WDNEmailTemplate.tpl.php
+++ lib/data/WDNEmailTemplate.tpl.php
@@ -1,37 +1,126 @@
-<div>
-<table cellspacing="0" cellpadding="0" border="0" width="98%" style="margin-top:10px; margin-bottom:10px;">
-<?php if (isset($context->web_view_uri)): ?>
-<tr>
-    <td align="center">
-        <a href="<?php echo $context->web_view_uri; ?>">Problem viewing? Click here to read online.</a>
-    </td>
-</tr>
-<?php endif; ?>
-<tr>
-    <td align="center" valign="top">
-        <!-- [ header starts here] -->
-        <table cellspacing="0" cellpadding="0" border="0" width="600">
-            <tr>
-                <td valign="top"><img src="http://www.unl.edu/wdn/templates_3.0/images/email/header.jpg" alt="The University of Nebraska-Lincoln" width="600" height="126" border="0"/></td>
-            </tr>
-        </table>
-        <table cellspacing="0" cellpadding="0" border="0" width="600" valign="top">
-            <tr>
-                <td width="12" bgcolor="#E0E0E0">&nbsp;</td>
-                <td width="10">&nbsp;</td>
-                <td align="left" valign="top" width="556">
-                    <?php echo $context->html_body; ?>
-                </td>
-                <td width="10"></td>
-                <td width="12" bgcolor="#E0E0E0"></td>
-            </tr>
-        </table>
-        <table cellspacing="0" cellpadding="0" border="0" width="600" height="22">
-            <tr>
-                <td valign="top"><img src="http://www.unl.edu/wdn/templates_3.0/images/email/footer.jpg" alt="The University of Nebraska-Lincoln" width="600" height="22" /></td>
-            </tr>
-        </table>
-    </td>
-</tr>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns:v="urn:schemas-microsoft-com:vml">
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    <style type="text/css">
+        @media screen and (max-width: 480px) {
+            table.emailwrapto100pc, img.emailwrapto100pc {
+                width: 100% !important;
+                height: auto !important;
+            }
+            span.emailbodytext, a.emailbodytext {
+                font-size: 16px !important;
+                line-height: 27px !important;
+            }
+            span.h1 {
+                font-size: 28px !important;
+            }
+            span.h2 {
+                font-size: 22px !important;
+            }
+            span.h3 {
+                font-size: 18px !important;
+            }
+            td.emailcolsplit {
+                width: 100% !important;
+                float: left !important;
+            }
+            td.emailcolgutter {
+                display: none;
+            }
+            a.emailmobbutton {
+                display:block !important;
+                font-size: 16px !important;
+                font-weight: bold !important;
+                padding: 6px 4px 8px 4px !important;
+                line-height: 18px !important;
+                background: #eeeeee !important;
+                border-radius:5px !important;
+                margin: 10px auto;
+                width: 70%;
+                text-align:center;
+                color: #111111;
+                text-decoration: none;
+                text-shadow: #ffffff 1px 0 0 ;
+            }
+        }
+        @media screen and (min-width: 480px) and (max-width: 1024px) {
+            table.emailwrapto100pc, img.emailwrapto100pc {
+                width:100% !important;
+                height:auto !important;
+            }
+            span.emailbodytext], a.emailbodytext {
+            font-size:16px !important;
+            line-height:21px !important;
+        }
+        span.h1 {
+            font-size: 28px !important;
+        }
+        span.h2 {
+            font-size: 22px !important;
+        }
+        span.h3 {
+            font-size: 18px !important;
+        }
+        a.emailmobbutton {
+            display:block !important;
+            font-size: 16px !important;
+            font-weight: bold !important;
+            padding: 6px 4px 8px 4px !important;
+            line-height: 18px !important;
+            background: #eeeeee !important;
+            border-radius:5px !important;
+            margin: 10px auto;
+            width: 70%;
+            text-align:center;
+            color: #111111;
+            text-decoration: none;
+            text-shadow: #ffffff 1px 0 0 ;
+        }
+            }
+    </style>
+</head>
+<body style="padding:0; margin:0;-webkit-text-size-adjust:none;-ms-text-size-adjust: 100%; background-color:#ffffff;" bgcolor="#ffffff">
+<table cellpadding="0" cellspacing="0" border="0" height="69" width="100%">
+    <tr>
+        <td background="http://www.unl.edu/ucomm/images/emails/top_bar.gif" bgcolor="#eeeeee" valign="top">
+            <!--[if gte mso 9]>
+            <v:rect xmlns:v="urn:schemas-microsoft-com:vml" fill="true" stroke="false" style="mso-width-percent:1000;height:69px;">
+                <v:fill type="tile" src="http://www.unl.edu/ucomm/images/emails/top_bar.gif" color="#eeeeee" />
+                <v:textbox inset="0,0,0,0">
+            <![endif]-->
+            <div>
+                <table width="640" border="0" cellspacing="0" cellpadding="0" class="emailwrapto100pc" align="center">
+                    <tr>
+                        <td>
+                            <img src="http://www.unl.edu/ucomm/images/emails/brand_icon.png" alt="Welcome to the University of Nebraska-Lincoln" width="261" height="69" border="0" style="display: block;" />
+                        </td>
+                    </tr>
+                </table>
+            </div>
+            <!--[if gte mso 9]>
+            </v:textbox>
+            </v:rect>
+            <![endif]-->
+        </td>
+    </tr>
+    <tr>
+        <td>
+            <table width="640" border="0" cellspacing="0" cellpadding="0" class="emailwrapto100pc" align="center">
+                <tr>
+                    <td width="640" height="15">
+                        <!-- Spacer area, sort of like top padding -->
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <!-- our content will go in here -->
+                        <?php echo $context->html_body; ?>
+                    </td>
+                </tr>
+            </table>
+        </td>
+    </tr>
 </table>
-</div>
+</body>
+</html>
