87797c840c30023f69663339d1821352f3c65c02
Michael Fairchild <mfairchild365@gmail.com>
2013-03-19 13:18:29 -0500

    Add a script to fix the queue

---
+++ scripts/fixQueue.php
@@ -0,0 +1,15 @@
+<?php
+require_once dirname(dirname(__FILE__)) . '/www/config.inc.php';
+
+/**
+ * This script will fix queue errors by expiring long-running jobs.
+ * 
+ * Jobs may become long-running if the processes is killed prematurly.
+ */
+
+//Only run a max of 5 checks at a time.
+$sql = "UPDATE assessment_runs SET status = 'error' WHERE timestampdiff(SECOND, date_started, now()) > ? AND status = 'running';";
+$sth = $db->prepare($sql);
+$sth->execute(array(UNL_WDN_Assessment::$timeout));
+
+echo $sth->rowCount() . " records updated." . PHP_EOL;
