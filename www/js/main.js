0e4ba9432d52e0773702766947db9865c0120ca8
Seth Meranda <seth@meranda.org>
2013-03-21 14:07:21 -0500

    Add Google Analytics tracking

--- www/js/main.js
+++ www/js/main.js
@@ -49,6 +49,7 @@ WDN.loadJQuery(function() {
 
                 if (history.pushState) {
                     history.pushState(null, null, '?uri=' + encodeURIComponent(uri));
+                    WDN.analytics.callTrackPageview(window.location);
                 }
                 
                 // This is a new query, so reset the waiting var
@@ -232,6 +233,16 @@ WDN.loadJQuery(function() {
 
     validator.initialize();
 });
+
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', 'UA-3203435-15']);
+_gaq.push(['_trackPageview']);
+
+(function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+})();
 
 // Handlebars helpers
 Handlebars.registerHelper('percentage', function (current, total) {
