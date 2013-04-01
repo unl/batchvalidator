1b5bfe266654d5d44cd54d7c5f1163ea5c89d884
Michael Fairchild <mfairchild365@gmail.com>
2013-04-01 16:48:09 -0500

    display the URL if we don't know the site title

--- www/templates/email/UNL/Assessment/Mailer.tpl.php
+++ www/templates/email/UNL/Assessment/Mailer.tpl.php
@@ -15,7 +15,7 @@ $ok_class = 'margin-bottom: 20px; padding: 20px; background:#b7dd9b; border-styl
     ?>
 </span>
 <span class="emailbodytext" style="margin-bottom: 30px; margin-left:30px; font-size:22px; line-height:34px; font-family:Helvetica,Arial,sans-serif; display:block;">
-    <a href="<?php echo $context->assessment->baseUri; ?>"><?php echo $stats['site_title']; ?></a>
+    <a href="<?php echo $context->assessment->baseUri; ?>"><?php echo ($stats['site_title'] == 'unknown')?$context->assessment->baseUri:$stats['site_title']; ?></a>
 </span>
 <span class="emailbodytext" style="margin-bottom: 30px; font-size:22px; line-height:34px; font-family:Helvetica,Arial,sans-serif; display:block;">
     Here are your results.
