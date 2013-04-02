e7e5e5592e3ea9eec88ee5d43881526837b0ea5d
Michael Fairchild <mfairchild365@gmail.com>
2013-04-02 13:58:52 -0500

    add a max queued constraint

--- scripts/auto_queue.php
+++ scripts/auto_queue.php
@@ -1,9 +1,26 @@
 <?php
 require_once dirname(dirname(__FILE__)) . '/www/config.inc.php';
 
-//Only run a max of 5 checks at a time.
-$sth = $db->prepare("select site.baseurl, date_completed from site LEFT JOIN assessment_runs on assessment_runs.baseurl = site.baseurl ORDER BY date_completed ASC LIMIT 5");
+const MAX_QUEUED = 10;
+
+$sth = $db->prepare("select count(*) as total from assessment_runs WHERE run_type='auto' AND status='queued'");
 $sth->execute();
+
+$result = $sth->fetch();
+
+if (isset($result['total']) && $result['total'] >= MAX_QUEUED) {
+    exit();
+}
+
+$limit = 0;
+
+if (isset($result['total'])) {
+    $limit = MAX_QUEUED - $result['total'];
+}
+
+//Only run a max of 10 checks at a time.
+$sth = $db->prepare("select site.baseurl, date_completed from site LEFT JOIN assessment_runs on assessment_runs.baseurl = site.baseurl ORDER BY date_completed ASC LIMIT ?");
+$sth->execute(array((int)$limit));
 
 while ($result = $sth->fetch()) {
     $assessment = new UNL_WDN_Assessment($result['baseurl'], $db);
