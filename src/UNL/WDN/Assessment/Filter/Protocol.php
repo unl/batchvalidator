ea1826e7b55bdc16d45b384d22af554d78818da6
Michael Fairchild <mfairchild365@gmail.com>
2013-04-05 16:50:14 -0500

    don't check for hash here

--- src/UNL/WDN/Assessment/Filter/Protocol.php
+++ src/UNL/WDN/Assessment/Filter/Protocol.php
@@ -6,7 +6,6 @@ class UNL_WDN_Assessment_Filter_Protocol extends Spider_UriFilterInterface
         //$this->current() contains the base uri, so the 'protocol' it probably won't start with the following.
         if (stripos($this->current(), 'javascript:') !== false
             || stripos($this->current(), 'tel:') !== false
-            || stripos($this->current(), '#') !== false
             || stripos($this->current(), 'mailto:') !== false) {
             return false;
         }
